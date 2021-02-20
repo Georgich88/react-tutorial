8146c32d3af9a69ce14e931c8aca512e28692704
igeo <georgeisaev@gmail.com>
2021-02-21 00:33:22 +0700

    3. Understanding the Base Features & Syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Max', age: Math.floor(Math.random() * 30) },
+      { name: 'Manu', age: Math.floor(Math.random() * 30) },
+      { name: 'Stepahnie', age: Math.floor(Math.random() * 30) }
+    ],
+    otherState: 'some other value'
+  };
+
+  switchNameHandler = (newName) => {
+    //console.log('Was clicked')
+    // DO NOT DO THIES: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: Math.floor(Math.random() * 30) },
+        { name: 'Manu', age: Math.floor(Math.random() * 30) },
+        { name: 'Stepahnie', age: Math.floor(Math.random() * 30) }
+      ]
+    })
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: Math.floor(Math.random() * 30) },
+        { name: event.target.value, age: Math.floor(Math.random() * 30) },
+        { name: 'Stepahnie', age: Math.floor(Math.random() * 30) }
+      ]
+    })
+  }
+
+  render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1x solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian')}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          changed={this.nameChangedHandler} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Maximus')}
+          changed={this.nameChangedHandler} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}> My Hobbies: Racing</Person>
+      </div>
+    )
+  };
+
 }
 
 export default App;
